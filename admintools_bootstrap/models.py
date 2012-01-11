14a0288b56253b95c81f9784e02fed25cb1d5112
Salvator <devnull@localhost>
2012-01-11 13:55:46 +0200

    #4 fixed admin filtered selects

--- admintools_bootstrap/models.py
+++ admintools_bootstrap/models.py
@@ -1,3 +1,14 @@
-from django.db import models
 
-# Create your models here.
+# monkey-pathching django admin
+
+from django.contrib.admin import widgets
+from django import forms
+
+class FilteredSelectMultiple(forms.SelectMultiple):
+    """
+        removing 2 select fields widget
+    """
+    def __init__(self, verbose_name, is_stacked, attrs=None, choices=[]):
+        super(FilteredSelectMultiple, self).__init__(attrs, choices)
+
+widgets.FilteredSelectMultiple = FilteredSelectMultiple
