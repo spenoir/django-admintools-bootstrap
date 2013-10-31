0d68e39008c87c122647d75a19f426384c8485f5
Adam <adam.spence@musicqubed.com>
2013-10-31 18:21:01 +0000

    moving bundles into init

--- admintools_bootstrap/settings.py
+++ admintools_bootstrap/settings.py
@@ -1,37 +1,4 @@
 from appconf import AppConf
-from django.conf import settings
-
-
-ADMIN_MEDIA_BUNDLES = (
-
-    ('admin.css',
-        'admintools_bootstrap/chosen/chosen.css',
-        'admintools_bootstrap/lib/bootstrap-datetimepicker.css',
-        'admintools_bootstrap/lib/bootstrap-fileupload.scss',
-        'admintools_bootstrap/sass/admin.scss',
-    ),
-
-    ('admin_dashboard.css',
-        'admintools_bootstrap/sass/dashboard.scss',
-    ),
-
-    ('admin.js',
-        'admintools_bootstrap/js/lazyload.js',
-        'admintools_bootstrap/js/jquery-1.9.1.js',
-        'admintools_bootstrap/js/jquery-ui-1.10.3.custom.js',
-        'admintools_bootstrap/js/json2.js',
-        'admin_tools/js/jquery/jquery.cookie.min.js',
-        'admin_tools/js/jquery/jquery.dashboard.js',
-        'admin_tools/js/menu.js',
-        'admintools_bootstrap/chosen/chosen.jquery.js',
-        'admintools_bootstrap/js/bootstrap/dropdown.js',
-        'admintools_bootstrap/js/bootstrap/alert.js',
-        'admintools_bootstrap/js/bootstrap-datetimepicker.js',
-        'admintools_bootstrap/js/bootstrap-fileupload.js',
-        'admintools_bootstrap/js/dismissAddAnotherPopup.js',
-        'admintools_bootstrap/js/admin.js',
-    ),
-)
 
 
 class AdminToolsBootstrapConf(AppConf):
