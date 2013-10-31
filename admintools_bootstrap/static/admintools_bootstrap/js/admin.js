bc00a4ac37374e965b61bb97d4738d0da24c930c
Adam <adam.spence@musicqubed.com>
2013-10-31 18:02:38 +0000

    fixing chosen select in actions

--- admintools_bootstrap/static/admintools_bootstrap/js/admin.js
+++ admintools_bootstrap/static/admintools_bootstrap/js/admin.js
@@ -1,8 +1,12 @@
 $(function(){
-		$('div:not(.action-wrapper) select').chosen({
+	$('select:not([name="action"])').chosen({
 		disable_search_threshold: 10,
 		width: "97%",
 		height: "34px"
+	});
+
+    $('select[name="action"]').chosen({
+		disable_search_threshold: 10
 	});
 
 	$(".form_datetime").datetimepicker({
